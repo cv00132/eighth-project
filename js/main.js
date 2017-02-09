b6e669926702b5d6b49878a22cb6dc51e55dec4d
Cory Vaughan <codercory13@gmail.com>
2017-02-09 18:16:46 -0500

    Fixed propagation to include variables, but still need to add all applicable ones.

--- js/main.js
+++ js/main.js
@@ -4,19 +4,34 @@
 //   return x.title;
 // });
 
-var productImg = function pictures(){
- for (i=0; i < orangeRose.results.length; i++){
-     $(".productGrid").append(`<div class="product"><img src = "${orangeRose.results[i].Images[0].url_170x135}"/></div>`);
-   }
- }
- productImg();
+// var productImg = function pictures(){
+//  for (i=0; i < orangeRose.results.length; i++){
+//      $(".productGrid").append(`<div class="product"><img src = "${orangeRose.results[i].Images[0].url_170x135}"/></div>`);
+//    }
+//  }
+//
+//  var productPrice = function pictures(){
+//   for (i=0; i < orangeRose.results.length; i++){
+//       $(".productGrid").append(`<div "${orangeRose.results[i].price}"</div>`);
+//     }
+//   }
 
- // var productPrice = function pictures(){
- //  for (i=0; i < orangeRose.results.length; i++){
- //      $(".productGrid").html(`<div "${orangeRose.results[i].price}"</div>`);
- //    }
- //  }
- //  productPrice();
+function getResults () {
+ for (i=0; i < orangeRose.results.length; i++){
+    var image = orangeRose.results[i].Images[0].url_170x135;
+    var title = orangeRose.results[i].title;
+    var shop = orangeRose.results[i].shop_name;
+    var price = orangeRose.results[i].price;
+
+    var itemResult = `<div class="product">
+
+    image title shop price
+
+    </div`
+  } $(".productGrid").append(itemResult);
+}
+
+getResults();
 
 // var productPrice = $(orangeRose.map(function(item) {
 //   return item.price;
